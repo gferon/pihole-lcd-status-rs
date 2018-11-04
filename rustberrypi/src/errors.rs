cff1ed42bd60a4be22af8a2e5f4faf44784078c8
Gabriel Feron <feron.gabriel@gmail.com>
2018-11-04 15:38:42 +0100

    Add Error trait to CommunicationError to enable usage of failure or error_chain

--- rustberrypi/src/errors.rs
+++ rustberrypi/src/errors.rs
@@ -1,6 +1,9 @@
 use bmp;
 use rppal::i2c;
 
+use std::fmt;
+use std::error;
+
 #[derive(Debug)]
 pub enum CommunicationError {
     BusError(i2c::Error),
@@ -8,4 +11,22 @@ pub enum CommunicationError {
     BitmapError(bmp::BmpError),
     ReadingError,
     WritingError,
+}
+
+impl fmt::Display for CommunicationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
+
+// This is important for other errors to wrap this one.
+impl error::Error for CommunicationError {
+    fn description(&self) -> &str {
+        "there was a communication error with a device connected to the Raspberry Pi."
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        // Generic error, underlying cause isn't tracked.
+        None
+    }
 }
